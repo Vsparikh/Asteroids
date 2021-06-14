10d2148f006e49a6045dcc2f82b4c48635a03cfa
Vrajang Parikh <vrajang@outlook.com>
2021-06-13 20:14:34 -0400

    Update ScoreTracker.cs
    Fix: score is displayed correctly at beginning

--- Assets/ScoreTracker.cs
+++ Assets/ScoreTracker.cs
@@ -13,7 +13,7 @@ public class ScoreTracker : MonoBehaviour
 
     private void Start()
     {
-        ScoreDisplay.text = Score.ToString();
+        ScoreDisplay.text = "Score: " + Score.ToString();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
